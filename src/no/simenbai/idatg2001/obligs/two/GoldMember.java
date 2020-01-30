38dc3730e9fa9a95f28cd38849dbc29a63e8a781
Simen Bai <github@simenbai.no>
2020-01-30 23:08:09 +0100

    Added gold memeber class

---
+++ src/no/simenbai/idatg2001/obligs/two/GoldMember.java
@@ -0,0 +1,15 @@
+package no.simenbai.idatg2001.obligs.two;
+
+import java.time.LocalDate;
+
+public class GoldMember extends BonusMember {
+    public GoldMember(int memberNo, Personals personals, LocalDate enrolledDate, int points) {
+        super(memberNo, personals, enrolledDate, points);
+    }
+
+    @Override
+    public void registerPoints(int points) {
+        super.registerPoints((int) (points * 1.5));
+    }
+}
+
